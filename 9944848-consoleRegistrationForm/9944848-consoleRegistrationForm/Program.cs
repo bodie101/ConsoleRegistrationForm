3f3f9699d0bca6d56997ca8a72fc8398b2253e62
9944848 <9944848@BP020365>
2019-04-09 11:00:05 +1200

    Classes added

--- 9944848-consoleRegistrationForm/9944848-consoleRegistrationForm/Program.cs
+++ 9944848-consoleRegistrationForm/9944848-consoleRegistrationForm/Program.cs
@@ -10,6 +10,58 @@ namespace _9944848_consoleRegistrationForm
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(userMessages.welcome);
+            Console.WriteLine();
+            Console.WriteLine(userMessages.pressanykey);
+            Console.ReadKey();
+
+            Console.Clear();
+
+
+            Console.Write(userMessages.firstNameQuestion);
+            string fname = Console.ReadLine();
+            Console.Clear();
+
+
+            Console.Write(userMessages.lastNameQuestion);
+            string lname = Console.ReadLine();
+            Console.Clear();
+
+            Console.Write(userMessages.emailQuestion);
+            string email = Console.ReadLine();
+            Console.Clear();
+
+
+            Console.Write(userMessages.passwordQuestion);
+            string pass1 = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write(userMessages.passwordConfirm);
+            string pass2 = Console.ReadLine();
+            Console.Clear();
+
+
+            Console.WriteLine(Validatepass.Passconfirm(pass1, pass2));
+            Console.ReadKey();
+
+            Console.Clear();
+
+
+            Console.WriteLine("Your Username is being automatically generated...");
+            int userName = GenerateUName.UserName();
+            Console.WriteLine();
+            Console.WriteLine(userMessages.pressanykey);
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Thankyou for Registering");
+            Console.WriteLine($"Your user name is : {userName} ");
+            Console.WriteLine($"Your password  is : {pass1} ");
+
+            Console.ReadKey();
+
+
         }
     }
 }
---
+++ 9944848-consoleRegistrationForm/9944848-consoleRegistrationForm/Validatepass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9944848_consoleRegistrationForm
+{
+    class Validatepass
+    {
+        public static string Passconfirm(string pass1, string pass2)
+        {
+            if (pass1 == pass2)
+            {
+                return "Password was set";
+            }
+            else
+            {
+                return "Password dosen't match";
+            }
+
+        }
+    }
+}
+
+
